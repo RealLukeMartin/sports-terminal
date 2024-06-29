21e741b7b03e82b8115d27605dcbd8b11fd2f90c
RealLukeMartin <lukemartinwebdev@gmail.com>
2024-06-28 20:48:07 -0500

    mlb box score foundation

--- internal/boxScores/mlbBoxScore.go
+++ internal/boxScores/mlbBoxScore.go
@@ -1,20 +1,43 @@
 package boxScores
 
-// type mlbTeamsBoxScoreStats struct {
-// 	name   string
-// 	runs   int
-// 	hits   int
-// 	errors int
-// }
+import tea "github.com/charmbracelet/bubbletea"
 
-// type inning struct {
-// 	homeScore int
-// 	awayScore int
-// }
+type mlbTeamsBoxScoreStats struct {
+	name   string
+	runs   int
+	hits   int
+	errors int
+}
 
-// type MlbBoxScoreModel struct {
-// 	homeStats   mlbTeamsBoxScoreStats
-// 	awayStats   mlbTeamsBoxScoreStats
-// 	homeInnings []inning
-// 	awayInnings []inning
-// }
+type inning struct {
+	homeScore int
+	awayScore int
+}
+
+type MlbBoxScoreModel struct {
+	homeStats   mlbTeamsBoxScoreStats
+	awayStats   mlbTeamsBoxScoreStats
+	homeInnings []inning
+	awayInnings []inning
+}
+
+func MlbBoxScoreInitialModel(homeStats mlbTeamsBoxScoreStats, awayStats mlbTeamsBoxScoreStats, homeInnings []inning, awayInnings []inning) MlbBoxScoreModel {
+	return MlbBoxScoreModel{
+		homeStats:   homeStats,
+		awayStats:   awayStats,
+		homeInnings: homeInnings,
+		awayInnings: awayInnings,
+	}
+}
+
+func (m MlbBoxScoreModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m MlbBoxScoreModel) Update() (tea.Msg, tea.Cmd) {
+	return m, nil
+}
+
+func (m MlbBoxScoreModel) View() string {
+	return "Box Scores Coming Soon..."
+}
